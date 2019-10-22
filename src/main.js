b98ec25238c64740485519aa1b81c024b2ad1ed0
Sergei <sergei.golds@gmail.com>
2019-10-22 22:50:02 +0300

    login validation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import Vuelidate from "vuelidate";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -7,6 +8,9 @@ import "materialize-css/dist/js/materialize.min";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+
+Vue.use(Vuelidate)
 
 Vue.filter("date", dateFilter)
 
