227c307bc0a9a51cd65f047261fe5915b9a03037
Sergei <sergei.golds@gmail.com>
2019-10-29 20:53:37 +0200

    registration page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,6 +2,7 @@
   <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
+
       <div class="input-field">
         <input
             id="email"
@@ -10,7 +11,6 @@
             :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
         >
         <label for="email">Email</label>
-
         <small 
           class="helper-text invalid"
           v-if="$v.email.$dirty && !$v.email.required"  
@@ -21,8 +21,8 @@
           v-else-if="$v.email.$dirty && !$v.email.email"  
         >Введите корректный Email
         </small>
-
       </div>
+
       <div class="input-field">
         <input
             id="password"
@@ -31,18 +31,16 @@
             :class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
         >
         <label for="password">Пароль</label>
-
         <small class="helper-text invalid"
         v-if="$v.password.$dirty && !$v.password.required"  
         >Введите пароль
         </small>
-
         <small class="helper-text invalid"
         v-else-if="$v.password.$dirty && !$v.password.minLength"  
         >Пароль должен быть {{$v.password.$params.minLength.min}} символов. Сейчас он {{password.length}}
         </small>
-
       </div>
+      
     </div>
     <div class="card-action">
       <div>
